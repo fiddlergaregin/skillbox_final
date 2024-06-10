a3539c5a19110bbabc455f267b7fd4df7f900013
Garegin <me@garegin.ru>
2024-06-10 20:07:29 +0300

    update 20240610

--- internal/app/app.go
+++ internal/app/app.go
@@ -10,3 +10,8 @@ func Run(cfg *config.Config) {
 	fmt.Println(cfg)
 
 }
+
+func parseCSV(filename string, num_fields int) (result string, err error) {
+	fmt.Println(num_fields)
+	return filename, nil
+}
